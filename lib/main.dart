899357cee12d091aa7c02fd7defd06b2dc594702
wwly <wwlyvip@163.com>
2020-04-08 22:44:40 +0800

    增加选中效果

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  static final model1 = ListTitleModel("应用信息");
+  static final model2 = ListTitleModel("消息推送", subTitles: [ListTitleModel("推送数据统计"), ListTitleModel("推送历史记录"), ListTitleModel("创建全量推送"), ListTitleModel("创建指定推送"), ListTitleModel("测试人员设置")]);
+  static final model3 = ListTitleModel("AB测试", subTitles: [ListTitleModel("AB测试记录")]);
+  final List<ListTitleModel> models = [model1, model2, model3];
+
+  ListTitleModel selectedModel;
+
   final List<List> list_titles = [ ["应用信息"], 
   ["消息推送", "推送数据统计", "推送历史记录", "创建全量推送", "创建指定推送", "测试人员设置"],
   ["AB测试", "AB测试记录"],
@@ -40,49 +47,107 @@ class _MyHomePageState extends State<MyHomePage> {
   ["用户来信活动", "用户来信活动"],
   ["旧版运营平台活动", "沙雕 AI 故事", "邀请好友安装", "人机翻译大赛"] ];
 
-  Drawer createDrawer(List titles) {
+  Drawer createDrawer(List<ListTitleModel> titles) {
     List<Widget> expansionTiles = [];
     var tempExpansionTile;
-    for (List item in titles) {
-      if (item.length > 1) {
-        final tempItem = item.sublist(1);
+    for (ListTitleModel item in titles) {
+      if (item.subTitles != null && item.subTitles.length > 0) {
+        final tempItem = item.subTitles;
         List<Widget> expansionTileChildren = [];
-        for (String tempTitle in tempItem) {
+        for (ListTitleModel tempTitle in tempItem) {
           final listTile = ListTile(
             leading: SizedBox(width: 0),
-            title: Text(tempTitle),
+            title: Text(tempTitle.title),
             trailing:Icon(Icons.keyboard_arrow_right),
           );
           expansionTileChildren.add(listTile);
         }
         tempExpansionTile = ExpansionTile(
-          title: Text(item.first),
+          title: Text(item.title),
           children: expansionTileChildren,
         );
       } else {
         tempExpansionTile = ListTile(
-          title: Text(item.first),
+          title: Text(item.title),
           trailing:Icon(Icons.keyboard_arrow_right),
         );
       }
       final tap = GestureDetector(
         child: tempExpansionTile,
         onTap: (){
-          if (tempExpansionTile is ListTile) {
-            
-          }
+          
         },);
-      expansionTiles.add(tempExpansionTile);
+      expansionTiles.add(tap);
       expansionTiles.add(Divider(height: 1.0));
     }
+
+    tapItem(ListTitleModel model) {
+      if (selectedModel != model) {
+        if (selectedModel != null) {
+          selectedModel.isSelected = false;
+        }
+        model.isSelected = true;
+        selectedModel = model;
+      }
+    }
+
+    Widget getRow(int i) {
+      //设置分割线
+      if (i.isOdd) return new Divider(height: 1,);
+      var temp;
+      ListTitleModel model = models[i];
+      if (model.subTitles != null && model.subTitles.length > 0) {
+        List<Widget> expansionTileChildren = [];
+        for (ListTitleModel tempTitle in model.subTitles) {
+          final listTile = ListTile(
+            leading: SizedBox(width: 0),
+            title: Text(tempTitle.title),
+            trailing:Icon(Icons.keyboard_arrow_right),
+            selected: tempTitle.isSelected,
+          );
+          final ges = GestureDetector(
+            child: listTile,
+            onTap: () {
+              setState(() {
+                tapItem(tempTitle);
+              });
+            },
+          );
+          expansionTileChildren.add(ges);
+        }
+        temp = ExpansionTile(
+          title: Text(model.title),
+          children: expansionTileChildren,
+        );
+      } else {
+        temp = ListTile(
+          title: Text(model.title),
+          trailing:Icon(Icons.keyboard_arrow_right),
+          selected: model.isSelected,
+        );
+      }
+      return GestureDetector(
+        child: temp,
+        onTap: (){
+          setState(() {
+            tapItem(model);
+          });
+        },);
+    }
+
+    ListView getListView() => ListView.builder(
+      itemCount: models.length,
+      itemBuilder: (BuildContext context, int position) {
+        return getRow(position);
+      }
+    );
+
     return Drawer(
-      child: ListView(
-        children: expansionTiles,
-      ),
+      child: getListView()
     );
   }
 
-  get _drawer => createDrawer(list_titles);
+  get _drawer => createDrawer(models);
 
   @override
   Widget build(BuildContext context) {
@@ -93,5 +158,18 @@ class _MyHomePageState extends State<MyHomePage> {
       drawer: _drawer,
       body: Center(child: Text("1")),
     );
+  }
+}
+
+
+class ListTitleModel {
+  String title;
+  List <ListTitleModel> subTitles;
+  bool isSelected;
+
+  ListTitleModel(this.title, {this.subTitles, this.isSelected = false});
+
+  test() {
+    print("-");
   }
 }
