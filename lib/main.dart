14dadf9307f3b6ee732edfe82ee7b0ae06ceb8bc
wwly_mbp <wwlyvip@163.com>
2020-04-08 18:45:10 +0800

    微调

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -18,15 +19,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -35,10 +27,18 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  List<List> list_titles = [ ["应用信息"], 
+  final List<List> list_titles = [ ["应用信息"], 
   ["消息推送", "推送数据统计", "推送历史记录", "创建全量推送", "创建指定推送", "测试人员设置"],
   ["AB测试", "AB测试记录"],
-  ["用户分群", "分群明细数据"] ];
+  ["用户分群", "用户分群任务", "分群明细数据"],
+  ["整体数据统计", "用户留存", "使用时长/频率", "流量跟踪"],
+  ["功能使用统计", "翻译字数统计", "网页翻译阅读数", "传送门阅读数", "看看阅读明细", "阅读时间分布", "图片翻译统计"],
+  ["付费数据统计", "整体数据", "会员数据", "补充包数据"],
+  ["小译工具箱", "用户/付费详查", "侧边栏活动设置", "弹框活动设置", "开屏广告设置", "激励视频广告设置"],
+  ["VIP 兑换码", "兑换码列表", "兑换码生成器"],
+  ["小译年度报告", "2019 年度报告", "活动/抽奖统计", "人物/书籍配置"],
+  ["用户来信活动", "用户来信活动"],
+  ["旧版运营平台活动", "沙雕 AI 故事", "邀请好友安装", "人机翻译大赛"] ];
 
   Drawer createDrawer(List titles) {
     List<Widget> expansionTiles = [];
@@ -51,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
           final listTile = ListTile(
             leading: SizedBox(width: 0),
             title: Text(tempTitle),
-            trailing:Icon(Icons.arrow_right)
+            trailing:Icon(Icons.keyboard_arrow_right),
           );
           expansionTileChildren.add(listTile);
         }
@@ -62,9 +62,16 @@ class _MyHomePageState extends State<MyHomePage> {
       } else {
         tempExpansionTile = ListTile(
           title: Text(item.first),
-          trailing:Icon(Icons.arrow_right),
+          trailing:Icon(Icons.keyboard_arrow_right),
         );
       }
+      final tap = GestureDetector(
+        child: tempExpansionTile,
+        onTap: (){
+          if (tempExpansionTile is ListTile) {
+            
+          }
+        },);
       expansionTiles.add(tempExpansionTile);
       expansionTiles.add(Divider(height: 1.0));
     }
